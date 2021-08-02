3afe3850daf54d89a04508421bd893add1c8ad2c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-08-02 21:10:25 +0200

    Update to support newer libs

---
+++ src/main/java/com/github/steveice10/mc/protocol/packet/ingame/client/player/ClientSwingArmPacket.java
@@ -0,0 +1,27 @@
+package com.github.steveice10.mc.protocol.packet.ingame.client.player;
+
+import com.github.steveice10.packetlib.io.NetInput;
+import com.github.steveice10.packetlib.io.NetOutput;
+import com.github.steveice10.packetlib.packet.Packet;
+
+import java.io.IOException;
+
+public class ClientSwingArmPacket implements Packet {
+
+    public ClientSwingArmPacket() {
+    }
+
+    @Override
+    public void read(NetInput in) throws IOException {
+    }
+
+    @Override
+    public void write(NetOutput out) throws IOException {
+    }
+
+    @Override
+    public boolean isPriority() {
+        return false;
+    }
+
+}
